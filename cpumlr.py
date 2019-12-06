fd9680925e8344afe31a41f965465f8aebc93af7
sowmiyavenkatesan <46124431+sowmiyavenkatesan@users.noreply.github.com>
2019-12-06 16:58:15 +0530

    Update cpumlr.py

--- cpumlr.py
+++ cpumlr.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import statsmodels.api as sm
 data = pd.read_csv("computer_hardware_dataset.csv")
 Xs = data.drop(['ERP','vendor_name','model_name','CACH','CHMIN','CHMAX'], axis=1)
 print(Xs)
